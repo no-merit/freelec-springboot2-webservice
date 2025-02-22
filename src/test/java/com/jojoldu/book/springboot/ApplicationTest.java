f55fb262d8a3e5a26761ec2eddff05339e84285a
no-merit <128347576+no-merit@users.noreply.github.com>
2025-02-22 14:13:44 +0900

    git revert 이후 다시 main 정리

--- src/test/java/com/jojoldu/book/springboot/ApplicationTest.java
+++ src/test/java/com/jojoldu/book/springboot/ApplicationTest.java
@@ -1,7 +1,11 @@
 package com.jojoldu.book.springboot;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ApplicationTest {
-
+    @Test
+    void contextLoads() {
+    }
 }
